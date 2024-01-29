991373722696b1cd71a9fe4a955a877fd8fa2ed6
Gabriele Viganò <gabriviga2005@gmail.com>
2024-01-29 19:34:16 +0100

    Implementata compilazione file

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace _30._2___Equilibrio_Gas
 {
@@ -15,13 +17,29 @@ namespace _30._2___Equilibrio_Gas
             int contA = 100;
             int contB = 0;
             Gas g = new Gas(nParticelle);
+            IniziaFile();
             for (int i = 0; i <= nIstanti; i++)
             {
                 Console.WriteLine("Evoluzione {0} - Settore A: {1} particelle - Settore B: {2} particelle ", i, contA, contB);
+                CompilaFile(i, contA, contB);
                 g.SimulaEvoluzione();
                 g.Risultati(ref contA, ref contB);     
             }
             Console.ReadKey();
+        }
+        static void CompilaFile(int istante, int contA, int contB)
+        {
+            File.AppendAllText("C:\\Users\\gabri\\OneDrive\\Desktop\\Gas.csv.txt", 
+            string.Format("{0}\t{1}\t{2}\r\n", istante, contA, contB));
+        }
+        static void IniziaFile()
+        {
+            if (File.Exists("C:\\Users\\gabri\\OneDrive\\Desktop\\Gas.csv.txt") == true)
+            {
+                File.Delete("C:\\Users\\gabri\\OneDrive\\Desktop\\Gas.csv.txt");
+            }
+            File.AppendAllText("C:\\Users\\gabri\\OneDrive\\Desktop\\Gas.csv.txt",
+            string.Format("Istante\tSettoreA\tSettoreB\r\n"));
         }
     }
 }
